1c15b4a8020744dbaf918e463cb49386add1c8be
centinna-l <jerryjoy99@gmail.com>
2021-05-22 20:33:39 +0530

    Get All Posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const auth = require('../middlewares/auth');
 
 
 exports.createPost = async (req, res) => {
@@ -43,5 +44,32 @@ exports.createPost = async (req, res) => {
                 });
             })
         });
+    });
+}
+//for only 1 ID 
+exports.getAllPosts = async (req, res) => {
+    let _id = req.uid;
+    let author_id = req.params.aid;
+    User.findById(_id, (err, huser) => {
+        if (err || !huser) {
+            return res.json({
+                "message": "Cannot Locate User - Please check if you have logged In"
+            });
+        }
+        User.findById({ _id: author_id }, (err, user) => {
+            if (err || !user) {
+                return res.json({
+                    "message": "Cannot locate Author"
+                });
+            }
+            return res.json(user.posts)
+
+        }).populate({
+            path: 'posts',
+            populate: {
+                path: 'comment'
+            }
+        });
+
     });
 }
--- routes/post.js
+++ routes/post.js
@@ -8,6 +8,7 @@ const auth = require("../middlewares/auth");
 const uploads = multer({ storage: storage });
 
 router.post('/create-post', uploads.single("post_image"), auth, postController.createPost);
-// router.get('/get-all-posts', uploads.none(), auth, postController.getAllPosts);
+//for a specefic User
+router.get('/get-all-posts/:aid', uploads.none(), auth, postController.getAllPosts);
 
 module.exports = router;
