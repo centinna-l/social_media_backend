f4ffc5ad2439135c040db2eb991c44af88f4d6c0
centinna-l <jerryjoy99@gmail.com>
2021-05-23 14:31:01 +0530

    Remove Followers and Following

--- controllers/listPeopleController.js
+++ controllers/listPeopleController.js
@@ -35,3 +35,66 @@ exports.getFollowingList = async (req, res) => {
         "following": user.following
     });
 }
+
+//remove a follower
+exports.removeFollower = async (req, res) => {
+    let _id = req.uid;
+    let follower_id = req.params.fid;
+    if (_id === follower_id) {
+        return res.json({
+            "message": "You cannot remove yourself"
+        });
+    }
+    User.findById(_id, (err, user) => {
+        if (err || !user) {
+            return res.json({
+                "message": "Cannot locate User / Token Invalid"
+            });
+        }
+        User.findByIdAndUpdate(_id, { $pull: { followers: follower_id } }, (err, result) => {
+            if (err || !result) {
+                return res.json({
+                    "message": "Cannot Remove User"
+                });
+            }
+            return res.json({
+                "message": "User Removed Sucessfully"
+            });
+        });
+    });
+}
+
+exports.removeFollowing = async (req, res) => {
+    let _id = req.uid;
+    let following_id = req.params.fid;
+    if (_id === following_id) {
+        return res.json({
+            "message": "You cannot remove yourself"
+        });
+    }
+    User.findById(_id, async (err, user) => {
+        if (err || !user) {
+            return res.json({
+                "message": "Cannot locate User / Token Invalid"
+            });
+        }
+        let check = await User.findOne({ _id }).select('following -_id').exec();
+        let temp_id = JSON.stringify(check.following[0]);
+        let temp1_id = JSON.stringify(following_id);
+        if (!(temp1_id === temp_id)) {
+            return res.json({
+                "message": "User Not found in Following list"
+            });
+        }
+        User.findByIdAndUpdate(_id, { $pull: { following: following_id } }, (err, result) => {
+            if (err || !result) {
+                return res.json({
+                    "message": "Cannot Remove User"
+                });
+            }
+            return res.json({
+                "message": "User Removed"
+            });
+        });
+    });
+}
--- routes/listPeople.js
+++ routes/listPeople.js
@@ -6,6 +6,8 @@ const peopleController = require('../controllers/listPeopleController');
 
 router.get('/get-followers-list', auth, peopleController.getFollowersList)
 router.get('/get-following-list', auth, peopleController.getFollowingList);
+router.delete('/remove-follower/:fid', auth, peopleController.removeFollower);
+router.delete('/remove-following/:fid', auth, peopleController.removeFollowing);
 
 
 module.exports = router;
